f180f32f2f2a8cd938ce1669622ec5816a0287b7
“anton-cornak” <anton.cornak@visma.com>
2023-09-30 23:29:57 +0200

    removed category

--- frontend/components/blog/DateCategories.tsx
+++ frontend/components/blog/DateCategories.tsx
@@ -2,15 +2,14 @@ import React from "react";
 // Misc
 import { format } from "date-fns";
 // Types and constants
-// import { Category } from "../shared/types";
+import { Category } from "../shared/types";
 
 const DateCategories = ({
 	date,
-	// categories,
 	textSize = "text-sm",
 }: {
 	date: string;
-	// categories?: Category[];
+	categories?: Category[];
 	textSize?: string;
 }): JSX.Element => {
 	return (
@@ -18,16 +17,6 @@ const DateCategories = ({
 			className={`${textSize} text-primary-500 dark:text-darkprimary-200 bg-primary-bg py-1 px-2 dark:bg-gray-800 rounded-md inline-block`}
 		>
 			<span>{format(new Date(date), "MMMM dd, yyyy")}</span>
-			{/* {categories && (
-				<>
-					|
-					<span className="ml-2">
-						{categories
-							.map((category: Category) => category.title)
-							.join(", ")}
-					</span>
-				</>
-			)} */}
 		</div>
 	);
 };
